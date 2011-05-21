ee66dd22cd69eabcd174f6750ea40769b8e75567
Nathan <nathan.borgo@gmail.com>
2011-05-21 16:08:16 -0500

    subscription class returns hash of values from the json response

--- lib/pinter/subscription.rb
+++ lib/pinter/subscription.rb
@@ -2,19 +2,16 @@ module Pinter
 	class Subscription
 
 		include HTTParty
-		base_uri = "http://www.pintpay.com/api/1"
-		default_params :output => 'json'
+		base_uri "http://www.pintpay.com/api/1"
+		default_params :api_key => Pinter.api_key, :api_secret => Pinter.api_secret
 	  format :json
 
-	  attr_reader :api_key, :api_secret
-
-		def initialize
-			@api_key = Pinter.api_key
-			@api_secret = Pinter.api_secret
+		def self.all
+			get "/subscriptions"
 		end
 
-		def self.list
-
+		def self.find(secret)
+			get "/subscriptions/#{secret}"
 		end
 
 	end
