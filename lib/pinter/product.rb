6883a1b2971554906abdcc02daf48500589a3f63
Nathan Borgo <nathan@subtractiv.com>
2011-05-22 11:39:46 -0500

    added Product class

---
+++ lib/pinter/product.rb
@@ -0,0 +1,13 @@
+module Pinter
+	class Product
+
+		include Base
+
+		attr_reader :name, :price_in_cents, :recurring, :created_at
+
+		def initialize(attributes)
+			set_instance_variables_from_hash attributes
+		end
+
+	end
+end
