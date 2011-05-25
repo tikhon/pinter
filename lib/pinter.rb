dc77aa5d8bae3537f22ab4826ff240ef0561c4ef
Nathan Borgo <nathan@subtractiv.com>
2011-05-25 09:48:31 -0500

    added Pinter.url to return the url of your pintpay page

--- lib/pinter.rb
+++ lib/pinter.rb
@@ -29,6 +29,20 @@ module Pinter
   def self.api_secret=(api_secret)
   	@api_secret = api_secret
   end
+
+  def self.subdomain
+    @subdomain
+  end
+
+  def self.subdomain=(subdomain)
+    @subdomain = subdomain
+  end
+
+  def self.url(options = {})
+    url = "https://#{Pinter.subdomain}.pintpay.com/"
+    url << "?#{options.to_params}" unless options.empty?
+    url
+  end
 end
 
 require 'pinter/hash'
